7ed8c684ae76edbd84d6c86d79e764da985a3485
Eduardo Zurbriggen <106617650+Eduzurbriggen14@users.noreply.github.com>
2023-11-15 23:04:38 -0300

    agregando cambio de estado en boton

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./TaskItem.css";
 
 function TaskItem(props) {
+
+  const [estado, setEstado]= useState({
+    marcado: false,
+  })
+
+  const marcarComoCompleto= ()=> {
+    setEstado({
+      marcado : !estado.marcado,
+    })
+  }
   return (
     <>
       <div className="item">
         {/* pongo la tarea que recibo por props en la etiqueta <p> */}
         <p>{props.item}</p>
-        <button>Tarea Completada</button>
+        <button onClick={marcarComoCompleto}>{estado.marcado ? "Tarea REALIZADA": "Tarea Pendiente"}</button>
       </div>
     </>
   );
 }
-
 export default TaskItem;
